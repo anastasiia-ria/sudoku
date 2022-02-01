0f967ac5cbccc400029d5a03a15af7b7d8f0f9eb
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-01 14:24:15 -0800

    have a good fail for Sudoku() object
    Co-authored-by Jake Haley <jakechaley@gmail.com>

---
+++ __test__/sudoku.test.js
@@ -0,0 +1,18 @@
+import Sudoku from "./../src/sudoku.js";
+
+describe("Sudoku", () => {
+  test("should corretcly create a sudoku object", () => {
+    const newSudoku = new Sudoku();
+    expect(newSudoku.board).toEqual([
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+  });
+});
