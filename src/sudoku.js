0a1196894f4d32aa87f4e7a1b589d4093c42a3d5
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-01 15:32:53 -0800

    pass test 1 for columnChecker()
    Co-authored-by Jake Haley <jakechaley@gmail.com>

--- src/sudoku.js
+++ src/sudoku.js
@@ -28,21 +28,20 @@ Sudoku.prototype.rowChecker = function () {
 };
 
 Sudoku.prototype.columnChecker = function () {
-  // let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  // let check = true;
-  // for (let i = 0; i < this.board.length; i++) {
-  //   let column = [];
-  //   for (let j = 0; j < this.board[i].length; j++) {
-  //     column.push(this.board[j][i]);
-  //   }
-  //   console.log(column);
-  //   column.sort();
-  //   for (let k = 0; k < this.board[i].length; k++) {
-  //     if (column[k] !== array[k]) {
-  //       check = false;
-  //       break;
-  //     }
-  //   }
-  // }
-  // return check;
+  let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let check = true;
+  for (let i = 0; i < this.board.length; i++) {
+    let column = [];
+    for (let j = 0; j < this.board[i].length; j++) {
+      column.push(this.board[j][i]);
+    }
+    column.sort();
+    for (let k = 0; k < this.board[i].length; k++) {
+      if (column[k] !== array[k]) {
+        check = false;
+        break;
+      }
+    }
+  }
+  return check;
 };
