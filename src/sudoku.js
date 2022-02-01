90a835a602db61da6af64562a6155cee1e69f9e2
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-01 15:02:22 -0800

    pass test1 for sudokuChecker()
    Co-authored-by Jake Haley <jakechaley@gmail.com>

--- src/sudoku.js
+++ src/sudoku.js
@@ -10,5 +10,19 @@ export default function Sudoku() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
-  Sudoku.prototype.sudokuChecker = function () {};
 }
+
+Sudoku.prototype.sudokuChecker = function () {
+  let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let check = true;
+  for (let i = 0; i < this.board.length; i++) {
+    this.board[i].sort();
+    for (let j = 0; j < this.board[i].length; j++) {
+      if (array[j] !== this.board[i][j]) {
+        check = false;
+        break;
+      }
+    }
+  }
+  return check;
+};
