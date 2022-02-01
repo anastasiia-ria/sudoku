ad52888271825fa61e13eca67641b4e75c4e9ae0
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-01 15:06:45 -0800

    pass test2 for sudokuChecker
    Co-authored-by Jake Haley <jakechaley@gmail.com>

--- __test__/sudoku.test.js
+++ __test__/sudoku.test.js
@@ -19,4 +19,20 @@ describe("Sudoku", () => {
     const newSudoku = new Sudoku();
     expect(newSudoku.sudokuChecker()).toEqual(false);
   });
+
+  test("should return true if the row repeats any number 1-9 more than once", () => {
+    const newSudoku = new Sudoku();
+    newSudoku.board = [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [9, 4, 7, 2, 1, 3, 8, 5, 6],
+      [9, 4, 7, 2, 1, 3, 8, 5, 6],
+      [5, 2, 3, 1, 9, 6, 8, 7, 4],
+      [9, 4, 7, 2, 1, 3, 8, 5, 6],
+      [6, 2, 5, 1, 4, 8, 9, 3, 7],
+      [5, 2, 3, 1, 9, 6, 8, 7, 4],
+      [9, 4, 7, 2, 1, 3, 8, 5, 6],
+      [6, 2, 5, 1, 4, 8, 9, 3, 7],
+    ];
+    expect(newSudoku.sudokuChecker()).toEqual(true);
+  });
 });
