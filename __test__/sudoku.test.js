7691d303c427e98c7d0e269b7074e3490cb84b7e
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-01 15:28:05 -0800

    update tests for rowChecker
    Co-authored-by Jake Haley <jakechaley@gmail.com>

--- __test__/sudoku.test.js
+++ __test__/sudoku.test.js
@@ -17,22 +17,22 @@ describe("Sudoku", () => {
   });
   test("should return false if the row repeats any number 1-9 more than once", () => {
     const newSudoku = new Sudoku();
-    expect(newSudoku.sudokuChecker()).toEqual(false);
+    expect(newSudoku.rowChecker()).toEqual(false);
   });
 
   test("should return true if the row repeats any number 1-9 more than once", () => {
     const newSudoku = new Sudoku();
     newSudoku.board = [
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [9, 4, 7, 2, 1, 3, 8, 5, 6],
-      [9, 4, 7, 2, 1, 3, 8, 5, 6],
-      [5, 2, 3, 1, 9, 6, 8, 7, 4],
-      [9, 4, 7, 2, 1, 3, 8, 5, 6],
-      [6, 2, 5, 1, 4, 8, 9, 3, 7],
-      [5, 2, 3, 1, 9, 6, 8, 7, 4],
-      [9, 4, 7, 2, 1, 3, 8, 5, 6],
-      [6, 2, 5, 1, 4, 8, 9, 3, 7],
+      [4, 3, 5, 2, 6, 9, 7, 8, 1],
+      [6, 8, 2, 5, 7, 1, 4, 9, 3],
+      [1, 9, 7, 8, 3, 4, 5, 6, 2],
+      [8, 2, 6, 1, 9, 5, 3, 4, 7],
+      [3, 7, 4, 6, 8, 2, 9, 1, 5],
+      [9, 5, 1, 7, 4, 3, 6, 2, 8],
+      [5, 1, 9, 3, 2, 6, 8, 7, 4],
+      [2, 4, 8, 9, 5, 7, 1, 3, 6],
+      [7, 6, 3, 4, 1, 8, 2, 5, 9],
     ];
-    expect(newSudoku.sudokuChecker()).toEqual(true);
+    expect(newSudoku.rowChecker()).toEqual(true);
   });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -12,7 +12,7 @@ export default function Sudoku() {
   ];
 }
 
-Sudoku.prototype.sudokuChecker = function () {
+Sudoku.prototype.rowChecker = function () {
   let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let check = true;
   for (let i = 0; i < this.board.length; i++) {
@@ -25,4 +25,24 @@ Sudoku.prototype.sudokuChecker = function () {
     }
   }
   return check;
+};
+
+Sudoku.prototype.columnChecker = function () {
+  // let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // let check = true;
+  // for (let i = 0; i < this.board.length; i++) {
+  //   let column = [];
+  //   for (let j = 0; j < this.board[i].length; j++) {
+  //     column.push(this.board[j][i]);
+  //   }
+  //   console.log(column);
+  //   column.sort();
+  //   for (let k = 0; k < this.board[i].length; k++) {
+  //     if (column[k] !== array[k]) {
+  //       check = false;
+  //       break;
+  //     }
+  //   }
+  // }
+  // return check;
 };
